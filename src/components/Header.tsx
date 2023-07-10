39848dd7bcd11e469bcf7f612508e98b67f72b7c
Matthew Duffy <mattduff707@hotmail.com>
2023-07-10 14:32:34 -0700

    Image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import dogpic from '../images/dogpic.jpg';
 
 const Wrapper = styled.div`
   display: flex;
@@ -23,11 +24,19 @@ const Title = styled.h2`
   letter-spacing: 2px;
   font-weight: 300;
 `;
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  border: 5px solid black;
+  margin-top: 12px;
+`;
 
 const Header = () => {
   return (
     <Wrapper>
       <Name>Matthew Duffy</Name>
+      <Image src={dogpic} />
       <Title>Full-Stack Engineer</Title>
     </Wrapper>
   );
