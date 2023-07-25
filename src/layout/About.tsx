52bb5dbc99b7c2f4cddcc79c0cc73347f7201803
Matthew Duffy <mattduff707@hotmail.com>
2023-07-25 13:26:08 -0700

    image count

--- src/layout/About.tsx
+++ src/layout/About.tsx
@@ -1,9 +1,9 @@
-import { css, styled } from "styled-components";
-import SectionBox from "../components/SectionBox";
-import { Heading, Text, Input } from "../components/generic";
-import { favorites, stack, tokens } from "../constants";
-import { useState } from "react";
-import Star from "../components/icons/Star";
+import { css, styled } from 'styled-components';
+import SectionBox from '../components/SectionBox';
+import { Heading, Text, Input } from '../components/generic';
+import { favorites, stack, tokens } from '../constants';
+import { useState } from 'react';
+import Star from '../components/icons/Star';
 
 const imgWidth = 180;
 const imgHeight = 180;
@@ -49,9 +49,7 @@ const StackItem = styled.li<{ isMatch: boolean; isSearch: boolean }>`
   font-weight: 600;
   transition: color 0.2s ease, opacity 0.2s ease;
   ${({ isMatch, isSearch }) => css`
-    color: ${isMatch && isSearch
-      ? "var(--color-primary)"
-      : "var(--font-color)"};
+    color: ${isMatch && isSearch ? 'var(--color-primary)' : 'var(--font-color)'};
     opacity: ${isMatch ? 1 : 0.5};
   `}
 `;
@@ -91,38 +89,34 @@ const StyledStar = styled(Star)`
   width: 20px;
   height: 20px;
   transition: fill 0.4s ease;
+  flex-shrink: 0;
 `;
 
 const About = ({ isDark }: { isDark: boolean }) => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   return (
-    <StyledSectionBox isDark={isDark} title={"About Me"}>
-      <div style={{ paddingBottom: "24px" }}>
+    <StyledSectionBox isDark={isDark} title={'About Me'}>
+      <div style={{ paddingBottom: '24px' }}>
         <Heading>As a Person</Heading>
         <Text withIndent>
-          A Midwesterner turned Pacific Northwest transplant. When I am not
-          click clacking on a keyboard to bring life to the web you can find me
-          tending to my garden, on a mindful nature walk, or kicking back and
-          watching the sun drift beyond the horizon. My fiancé and I enjoy a
-          relaxing day at home with our two puppies as much as we love a
-          spontaneous vacation to a new locale. Let's talk about music, movies,
-          and food!
+          A Midwesterner turned Pacific Northwest transplant. When I am not click clacking on a keyboard to bring life
+          to the web you can find me tending to my garden, on a mindful nature walk, or kicking back and watching the
+          sun drift beyond the horizon. My fiancé and I enjoy a relaxing day at home with our two puppies as much as we
+          love a spontaneous vacation to a new locale. Let's talk about music, movies, and food!
         </Text>
       </div>
-      <div style={{ paddingBottom: "24px" }}>
+      <div style={{ paddingBottom: '24px' }}>
         <Heading>As an Engineer</Heading>
         <Text withIndent>
-          A proud self taught developer who fears no technical challenge. I am
-          equal parts independent productivity machine and patiently involved
-          collaborator. I revel in delivering results and overcoming roadblocks
-          along the way. I am always open to feedback as I have found no greater
-          source of knowledge than that of my colleagues. Let's create something
-          meaningful!
+          A proud self taught developer who fears no technical challenge. I am equal parts independent productivity
+          machine and patiently involved collaborator. I revel in delivering results and overcoming roadblocks along the
+          way. I am always open to feedback as I have found no greater source of knowledge than that of my colleagues.
+          Let's create something meaningful!
         </Text>
       </div>
       <StackWrap>
@@ -133,7 +127,7 @@ const About = ({ isDark }: { isDark: boolean }) => {
         </InputWrap>
         <StyledText>
           Favorites
-          <span style={{ color: "var(--color-primary)" }}>
+          <span style={{ color: 'var(--color-primary)' }}>
             <StyledStar />
           </span>
         </StyledText>
